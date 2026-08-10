256e6ae29da464f73f76fe248af96581fc70aab4
Joseph <joseph.hansjpedronan@gmail.com>
2026-08-09 18:54:03 -0700

    48

---
+++ modules/network/terraform.tfvars
@@ -0,0 +1,3 @@
+gcp_project = "devops-project-505023"
+key_path = "C:\\Users\\hansj\\Downloads\\terraform-course-key.json"
+project_zone = "us-central1-a"
---
+++ modules/network/variables.tf
@@ -0,0 +1,36 @@
+variable "gcp_project" {
+    type = string
+}
+
+variable "key_path" {
+    type = string
+}
+
+variable "project_zone" {
+    type = string
+}
+
+variable "network_name" {
+    type = string
+    default = "my-network"
+}
+
+variable "subnetwork_name" {
+    type = string
+    default = "my-subnetwork"
+}
+
+variable "subnet_region" {
+    type = string
+    default = "us-central1"
+}
+
+variable "" {
+    type = 
+    default = 
+}
+
+variable "" {
+    type = 
+    default = 
+}
